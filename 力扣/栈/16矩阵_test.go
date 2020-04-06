901f7acef60e2a7b49fc2df608ab165ee7612dcd
Supo <306919454@qq.com>
2020-04-07 00:36:10 +0800

    矩阵

---
+++ 力扣/栈/16矩阵_test.go
@@ -0,0 +1,24 @@
+package 栈
+
+import "testing"
+
+/**
+给定一个由 0 和 1 组成的矩阵，找出每个元素到最近的 0 的距离。
+
+两个相邻元素间的距离为 1 。
+
+https://leetcode-cn.com/explore/learn/card/queue-stack/220/conclusion/892/
+*/
+
+type PositionUpdateMatrix struct {
+	X int
+	Y int
+}
+
+func updateMatrix(matrix [][]int) [][]int {
+
+}
+
+func Test_updateMatrix(t *testing.T) {
+
+}
