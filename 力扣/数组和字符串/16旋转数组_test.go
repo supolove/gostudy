ada6572ed34545ce125260e328a5608af0ad7204
Supo <306919454@qq.com>
2020-04-27 23:05:42 +0800

    添加三次反转法

--- 力扣/数组和字符串/16旋转数组_test.go
+++ 力扣/数组和字符串/16旋转数组_test.go
@@ -1,7 +1,6 @@
 package 数组和字符串
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -18,13 +17,46 @@ func rotate(nums []int, k int) {
 	for i := 0; i < len(nums); i++ {
 		n = append(n, nums[i])
 	}
-	o := n[k-1:]
-	p := n[:k-1]
 
-	fmt.Println(o)
-	fmt.Println(p)
+	o := n[:len(nums)-k]
+	p := n[len(nums)-k:]
+
+	for i := 0; i < len(nums); i++ {
+		if i < len(p) {
+			nums[i] = p[i]
+		} else {
+			nums[i] = o[i-len(p)]
+		}
+	}
+}
+
+// 三次反转做法
+
+func rotate2(nums []int, k int) {
+
+	reverse := func(nums []int, start, end int) {
+		for start < end {
+			nums[start], nums[end] = nums[end], nums[start]
+			start++
+			end--
+		}
+	}
+
+	k %= len(nums)
+	// 第一反转
+	reverse(nums, 0, len(nums)-1)
+	// 第二次
+	reverse(nums, 0, k-1)
+	// 第三次
+	reverse(nums, k, len(nums)-1)
 }
 
 func Test_rotate(t *testing.T) {
-	rotate([]int{1, 2, 3, 4, 5, 6}, 2)
+	rotate([]int{1, 2, 3, 4, 5}, 3)
+	/*
+		5 2 3 4 1
+		5 4 3 2 1
+		4 5 3 2 1
+		4 5 1 2 3
+	*/
 }
