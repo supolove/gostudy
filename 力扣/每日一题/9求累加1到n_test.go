5dbb6a6e9e9fbe07040e868639aa855d05bbf44a
supolovedn <306919454@qq.com>
2020-06-02 11:42:32 +0800

    每日一题

---
+++ 力扣/每日一题/9求累加1到n_test.go
@@ -0,0 +1,32 @@
+package 每日一题
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+/*
+https://leetcode-cn.com/problems/qiu-12n-lcof/
+求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）
+*/
+
+func sumNums(n int) int {
+	return (int(math.Pow(float64(n), 2.0)) + n) >> 1
+}
+
+func sumNums2(n int) int {
+	ans := 0
+	var sum func(int) bool
+	sum = func(n int) bool {
+		ans += n
+		return n > 0 && sum(n-1)
+	}
+	sum(n)
+	return ans
+}
+
+func Test_sumNums(t *testing.T) {
+	fmt.Println(12 >> 1)
+	fmt.Println(sumNums(3))
+}
