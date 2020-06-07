16c7a58b533e1bab17b9bff723d0f21212b7df26
supo <306919454@qq.com>
2020-06-07 14:34:31 +0800

    难

---
+++ 力扣/每日一题/14单词接龙II_test.go
@@ -0,0 +1,37 @@
+package 每日一题
+
+import "testing"
+
+/*
+https://leetcode-cn.com/problems/word-ladder-ii/
+
+*/
+
+func findLadders(beginWord string, endWord string, wordList []string) [][]string {
+
+	var ans [][]string
+
+	containWord := false
+	m := map[string]bool{}
+	for _, word := range wordList {
+		if word == endWord {
+			containWord = true
+			m[word] = true
+		}
+	}
+	if !containWord {
+		return ans
+	}
+	return ans
+}
+
+func bfs(beginWord, endWord string, wordList map[string]bool, ans [][]string) {
+	//var queue []string
+	//var mpath []string
+	//mpath = append(mpath,beginWord)
+
+}
+
+func Test_findLadders(t *testing.T) {
+
+}
